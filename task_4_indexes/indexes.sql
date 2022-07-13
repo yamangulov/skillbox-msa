21f77c957ece4f390a0f7e0b8d43c0fd4292f6e5
YamangulovAN <yamangulov@gmail.com>
2022-07-13 09:48:11 +0300

    task_4_indexes работа с замечаниями преподавателя

--- task_4_indexes/indexes.sql
+++ task_4_indexes/indexes.sql
@@ -1,5 +1,5 @@
-create index if not exists sex_name_idx on users_scheme.sex using hash (name);
+create index if not exists gender_id_idx on users_scheme.profile using hash (gender_id);
 
-create index if not exists city_name_idx on users_scheme.sex using hash (name);
+create index if not exists city_id_idx on users_scheme.profile using hash (city_id);
 
-create index if not exists sex_id_city_id_idx on users_scheme.profile using hash (sex_id, city_id);
+create index if not exists gender_id_city_id_idx on users_scheme.profile (gender_id, city_id);
