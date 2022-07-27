19aaee8843e1d2e7e60686d10fc81c7c3bc83392
YamangulovAN <yamangulov@gmail.com>
2022-07-27 17:29:57 +0300

    ДЗ "Написать endpoint" доработано, добавлен swagger, на проверку

---
+++ task_5_endpoint/src/main/java/com/yamangulov/endpoint/config/SpringfoxConfig.java
@@ -0,0 +1,81 @@
+package com.yamangulov.endpoint.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SpringfoxConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yamangulov.endpoint.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Service Bus",
+                "Skillbox - microservice architecture course project",
+                "0.0.1",
+                "TERMS OF SERVICE URL",
+                new Contact("Andrey Iamangulov","localhost","yamangulov@gmail.com"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
+    }
+
+    // для установления совместимости springfox-boot-starter 3.0.0 и spring-boot-starter-parent >2.6.0
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+
+}
--- task_5_endpoint/src/main/java/com/yamangulov/endpoint/controller/UserController.java
+++ task_5_endpoint/src/main/java/com/yamangulov/endpoint/controller/UserController.java
@@ -2,6 +2,8 @@ package com.yamangulov.endpoint.controller;
 
 import com.yamangulov.endpoint.entity.User;
 import com.yamangulov.endpoint.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Api
 public class UserController {
 
     private final UserService userService;
@@ -18,26 +21,31 @@ public class UserController {
     }
 
     @PostMapping
+    @ApiOperation("Создание пользователя")
     public String createUser(@RequestParam Map<String, String> requestParams) {
         return userService.creatUser(requestParams);
     }
 
     @PutMapping(path = "/{id}")
+    @ApiOperation("Обновление данных пользователя")
     public String updateUser(@RequestParam Map<String, String> requestParams, @PathVariable UUID id) {
         return userService.updateUser(requestParams, id);
     }
 
     @GetMapping(path = "/{id}")
+    @ApiOperation("Получение данных пользователя")
     public User getUser(@PathVariable UUID id) {
         return userService.getUser(id);
     }
 
     @DeleteMapping(path = "/{id}")
+    @ApiOperation("Удаление пользователя")
     public String deleteUser(@PathVariable UUID id) {
         return userService.deleteUser(id);
     }
 
     @PostMapping("/subscribe/{subscriber_id}/{subscribed_id}")
+    @ApiOperation("Подписка пользователя на сообщения другого пользователя")
     public String subscribe(
             @PathVariable UUID subscriber_id,
             @PathVariable UUID subscribed_id) {
@@ -45,6 +53,7 @@ public class UserController {
     }
 
     @PostMapping("/unsubscribe/{subscriber_id}/{subscribed_id}")
+    @ApiOperation("Отписка пользователя от сообщений другого пользователя")
     public String unsubscribe(
             @PathVariable UUID subscriber_id,
             @PathVariable UUID subscribed_id) {
